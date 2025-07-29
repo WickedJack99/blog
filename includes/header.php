6c9525c8a3edb7dd3b5d82cfa86e6409dc923acc
WickedJack99 <wickedjack99.bkp0n@passmail.net>
2025-07-29 11:28:06 +0200

    Removed ugly .php link.

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
     <nav>
         <ul class="navbar">
             <li><a href="/">🏠 Home</a></li>
-            <li><a href="free_cookies.php" target="_blank" rel="noopener noreferrer" title="YouTube will see your IP if you open 👀">🍪 Free cookies!</a></li>
+            <li><a href="free_cookies" target="_blank" rel="noopener noreferrer" title="YouTube will see your IP if you open 👀">🍪 Free cookies!</a></li>
             <li><a href="https://github.com/WickedJack99/blog" target="_blank" rel="noopener noreferrer" title="GitHub will see your IP if you open 👀">🧑‍💻 GitHub</a></li>
         </ul>
     </nav>
